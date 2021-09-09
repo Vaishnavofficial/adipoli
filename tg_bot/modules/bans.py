7c6493de08aafa2ee80c27a5193a847ba372ef85
Issac newton <86590743+DashamoolamDaamu@users.noreply.github.com>
2021-09-09 11:02:25 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -91,7 +91,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         chat.kick_member(user_id)
         bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         keyboard = []
-        reply = "{} ന് ബണ്ണ് കൊടുത്തു വിട്ടിട്ടുണ്ട് !".format(mention_html(member.user.id, member.user.first_name))
+        reply = "{} has been banned !".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
@@ -100,7 +100,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
             chat_id = update.effective_chat.id
             message = update.effective_message
             # Do not reply
-            reply = "{} ന് ബണ്ണ് കൊടുത്തു വിട്ടിട്ടുണ്ട് !".format(mention_html(member.user.id, member.user.first_name))
+            reply = "{} has been banned !".format(mention_html(member.user.id, member.user.first_name))
             bot.send_message(chat_id, reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
 #           message.reply_text('Banned!', quote=False)
             return log
